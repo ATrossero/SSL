730e2e03254ce23d05b21e7e209644f088ce9d43
ATrossero <83141246+ATrossero@users.noreply.github.com>
2023-04-21 19:35:44 -0300

    Update main.c

--- 00.HelloWorld/main.c
+++ 00.HelloWorld/main.c
@@ -4,9 +4,5 @@
 int main()
 {
     printf("Hello, World!\n");
-    FILE *archivo = freopen("output.txt", "w", stdout);
-    printf("Hello, World!\n");
-    fclose(archivo);
-    freopen("/dev/tty", "w", stdout);
     return 0;
 }
